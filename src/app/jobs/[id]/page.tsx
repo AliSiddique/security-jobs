57d6e62f17245abd437738f65efa43d2c721f303
AliSiddique <alisiddique10@hotmail.com>
2024-05-06 23:52:30 +0100

    A

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -37,7 +37,7 @@ export default async function page({ params }: Props) {
           </div>
           <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
             <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
-              <h1 className="truncate text-2xl font-bold text-gray-900">{job.title} at {job.company_name}</h1>
+              <h1 className="truncate text-2xl font-bold text-gray-900">{job.title} at <Link className='underline' target='_blank' href={job.company_website}>{job.company_name}</Link></h1>
             </div>
             <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
               {/* <button
@@ -57,21 +57,137 @@ export default async function page({ params }: Props) {
             </div>
           </div>
         </div>
-        <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
-          <h1 className="truncate text-2xl font-bold text-gray-900">{job.title}</h1>
+        
+        <div className="mt-6  min-w-0 flex-1 sm:block ">
+        <dl className='mt-12 grid grid-cols-2 gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-16 lg:grid-cols-5'>
+              <div className='flex flex-col gap-y-3 border-l pl-6'>
+                <dt className='inline-flex items-center gap-2 text-base leading-7 text-slate-500'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='icon icon-tabler icon-tabler-map-pin'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                    <path d='M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0'></path>
+                    <path d='M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z'></path>
+                  </svg>
+                  Location
+                </dt>
+                <dd className='text-base font-semibold tracking-tight text-slate-900'>
+                  {job.location}
+                </dd>
+              </div>
+              <div className='flex flex-col gap-y-3 border-l pl-6'>
+                <dt className='inline-flex items-center gap-2 text-base leading-7 text-slate-500'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='icon icon-tabler icon-tabler-activity'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                    <path d='M3 12h4l3 8l4 -16l3 8h4'></path>
+                  </svg>
+                  Department
+                </dt>
+                <dd className='text-base font-semibold tracking-tight text-slate-900'>
+                 {job.department}
+                </dd>
+              </div>
+              <div className='flex flex-col gap-y-3 border-l pl-6'>
+                <dt className='inline-flex items-center gap-2 text-base leading-7 text-slate-500'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='icon icon-tabler icon-tabler-stack-pop'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                    <path d='M7 9.5l-3 1.5l8 4l8 -4l-3 -1.5'></path>
+                    <path d='M4 15l8 4l8 -4'></path>
+                    <path d='M12 11v-7'></path>
+                    <path d='M9 7l3 -3l3 3'></path>
+                  </svg>
+                  Level
+                </dt>
+                <dd className='text-base font-semibold tracking-tight text-slate-900'>
+                  Senior
+                </dd>
+              </div>
+              <div className='flex flex-col gap-y-3 border-l pl-6'>
+                <dt className='inline-flex items-center gap-2 text-base leading-7 text-slate-500'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='icon icon-tabler icon-tabler-clock-bolt'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                    <path d='M20.984 12.53a9 9 0 1 0 -7.552 8.355'></path>
+                    <path d='M12 7v5l3 3'></path>
+                    <path d='M19 16l-2 3h4l-2 3'></path>
+                  </svg>
+                  Type
+                </dt>
+                <dd className='text-base font-semibold tracking-tight text-slate-900'>
+                  {job.type}
+                </dd>
+              </div>
+              <div className='flex flex-col gap-y-3 border-l pl-6'>
+                <dt className='inline-flex items-center gap-2 text-base leading-7 text-slate-500'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='icon icon-tabler icon-tabler-coin'
+                    width='20'
+                    height='20'
+                    viewBox='0 0 24 24'
+                    stroke-width='2'
+                    stroke='currentColor'
+                    fill='none'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+                    <path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0'></path>
+                    <path d='M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1'></path>
+                    <path d='M12 7v10'></path>
+                  </svg>
+                  Salary
+                </dt>
+                <dd className='text-base font-semibold tracking-tight text-slate-900'>
+                  £{job.salary}
+                </dd>
+              </div>
+            </dl>
         </div>
       </div>
     </div>
-        <div className='mx-auto max-w-7xl px-8 py-12 md:px-32'>
-            <ul className='divide-y divide-slate-100 w-full'>
-                <li className='flex flex-col gap-2'>
-                    <h3 className='text-lg font-semibold leading-6 text-slate-900 lg:text-xl'>
-                    </h3>
-                    <p className='text-xl text-gray-500'>
-                    {job.location} | {job.type} | {job.salary}
-                    </p>
-                </li>
-            </ul>
+        <div className='mx-auto max-w-7xl px-16 py-12 md:px-40'>
           <div className=''>
             <main>
               <section>
@@ -99,16 +215,12 @@ export default async function page({ params }: Props) {
             </main>
           </div>
           <div className='py-6'>
-            {/* <a
-              href={job.apply_link ?? ''}
-              className='inline-flex w-full items-center rounded-full  px-5 py-3 text-sm leading-4 text-white duration-200 hover:bg-purple-50 hover:text-purple-500 focus:outline-none focus:ring-0 focus:ring-purple-500 focus:ring-offset-2 md:w-auto md:focus:ring-2'
-            >
-              Apply now
-            </a> */}
+
             <Button asChild
             >
                 <Link
               href={job.apply_link ?? ''}
+              target='_blank'
               className='inline-flex w-full items-center rounded-full  px-5 py-3 text-sm leading-4 text-white duration-200 hover:bg-purple-50 hover:text-purple-500 focus:outline-none focus:ring-0 focus:ring-purple-500 focus:ring-offset-2 md:w-auto md:focus:ring-2'
             >
               Apply now
